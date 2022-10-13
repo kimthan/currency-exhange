18d7b20ac99c4d879ea07a829c450fb02ce7c4cb
KT <kt@KTs-MacBook-Air.T-mobile.com>
2022-10-13 09:37:02 -0700

    added api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState({});
+
+  async function fetchData() {
+    const response = await fetch("https://open.er-api.com/v6/latest/USD");
+    const data = await response.json();
+    setData(() => data.rates);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  function handleChange(e) {
+    console.log(e.target.value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-screen flex items-center justify-center">
+      <div className="border-2 border-black w-[200px] h-[200px] text-center">
+        currency exchange
+        <div className="">
+          <input className="text-center" placeholder={"enter amount"} />
+        </div>
+        <select onChange={handleChange}>
+          {Object.keys(data).map((currency) => {
+            return (
+              <option key={currency} value={currency}>
+                {currency}
+              </option>
+            );
+          })}
+        </select>
+        <select onChange={handleChange}>
+          <option>To</option>
+          <option value="A">Apple</option>
+          <option value="B">Banana</option>
+          <option value="C">Cranberry</option>
+        </select>
+        <div className="">output</div>
+      </div>
+      <div></div>
     </div>
   );
 }
