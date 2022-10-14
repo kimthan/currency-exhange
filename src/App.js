c14232bfdc5d5a28010b1dd714c180740782d6dc
KT <kt@KTs-MacBook-Air.T-mobile.com>
2022-10-13 20:57:59 -0700

    added api

--- src/App.js
+++ src/App.js
@@ -1,44 +1,83 @@
 import { useState, useEffect } from "react";
 
+const myCurrency = ["USD", "SEK", "JPY"];
+
 function App() {
   const [data, setData] = useState({});
-
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
+  const [amount, setAmount] = useState(0);
   async function fetchData() {
     const response = await fetch("https://open.er-api.com/v6/latest/USD");
     const data = await response.json();
     setData(() => data.rates);
   }
+  const output = data[from] * amount * data[to] || 0;
+  console.log(output);
 
   useEffect(() => {
     fetchData();
   }, []);
 
   function handleChange(e) {
-    console.log(e.target.value);
+    if (e.target.id === "from") {
+      setFrom(() => e.target.value);
+    }
+    if (e.target.id === "to") {
+      setTo(() => e.target.value);
+    }
+  }
+
+  function handleAmount(e) {
+    setAmount(() => e.target.value);
   }
   return (
     <div className="App h-screen flex items-center justify-center">
-      <div className="border-2 border-black w-[200px] h-[200px] text-center">
+      <div className="border-2 bg-[#4287f5] w-[200px] h-[200px] text-center">
         currency exchange
         <div className="">
-          <input className="text-center" placeholder={"enter amount"} />
+          <input
+            value={amount}
+            onChange={handleAmount}
+            className="text-center m-2"
+            placeholder={"enter amount"}
+          />
         </div>
-        <select onChange={handleChange}>
-          {Object.keys(data).map((currency) => {
+        <select id={"from"} className="m-2" onChange={handleChange}>
+          <option>From</option>
+          {myCurrency.map((x) => {
+            return (
+              <option key={x} value={x}>
+                {x}
+              </option>
+            );
+          })}
+          {/* {Object.keys(data).map((currency) => {
             return (
               <option key={currency} value={currency}>
                 {currency}
               </option>
             );
+          })} */}
+        </select>
+        <select id={"to"} onChange={handleChange}>
+          <option>To</option>
+          {/* {Object.keys(data).map((currency) => {
+            return (
+              <option key={currency} value={currency}>
+                {currency}
+              </option>
+            );
+          })} */}
+          {myCurrency.map((x) => {
+            return (
+              <option key={x} value={x}>
+                {x}
+              </option>
+            );
           })}
         </select>
-        <select onChange={handleChange}>
-          <option>To</option>
-          <option value="A">Apple</option>
-          <option value="B">Banana</option>
-          <option value="C">Cranberry</option>
-        </select>
-        <div className="">output</div>
+        <div className="mt-4 text-5xl">{output.toFixed(2)}</div>
       </div>
       <div></div>
     </div>
